ad62be1a79fc07ba597ad5b65c879a7cca884742
zhanglindeng <zhanglindeng@outlook.com>
2018-01-20 14:59:30 +0800

    bootstrap 4

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -6,18 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  username: string;
+  email: string;
   password: string;
+  passwordEmpty = false;
+  emailEmpty = false;
+  requesting = false;
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
+    this.email = '';
+    this.password = '';
   }
 
   login() {
-    console.log(this.username);
-    console.log(this.password);
+    // email
+    if (this.email.length === 0) {
+      this.emailEmpty = true;
+      return;
+    }
+    this.emailEmpty = false;
+
+    // password
+    if (this.password.length === 0) {
+      this.passwordEmpty = true;
+      return;
+    }
+    this.passwordEmpty = false;
+
+    // http request
+    this.requesting = true;
+
+    this.http.post(`${environment.apiUrl}/login`, {
+      email: this.email,
+      password: this.password,
+    }).subscribe(
+      data => {
+        console.log(data);
+        this.requesting = false;
+      },
+      err => {
+        alert(err.statusText);
+        this.requesting = false;
+      }
+    );
   }
 
 }
